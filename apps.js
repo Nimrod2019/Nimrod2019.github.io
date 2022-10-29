0bf3f733298aa6531531e04ca364b953cdb3a806
Nimrod2019 <47126482+Nimrod2019@users.noreply.github.com>
2022-10-28 20:20:28 -0400

    Add files via upload

---
+++ apps.js
@@ -0,0 +1,31 @@
+async function PVPBattles(){
+	const api_link='https://api.worldofwarships.com/wows/account/info/?application_id=40c89c194464b943f74c5d5044cc66ed&account_id=1008221991%2C+1011213121%2C+1016093123%2C+1003075437';
+	const response=await fetch(api_link)
+	const datapoints=await response.json();
+	for (let account in datapoints.data){
+		BattlesPlayedPVP.push(datapoints.data[account]["statistics"]["pvp"]["battles"]);
+		PlayerFullName.push(datapoints.data[account]["nickname"]);
+	}
+}
+
+async function SurvivedBattles(){
+	const api_link='https://api.worldofwarships.com/wows/account/statsbydate/?application_id=40c89c194464b943f74c5d5044cc66ed&account_id=1008221991&dates=20220921%2C+20220922%2C+20220923%2C+20220924%2C+20220925';
+	const response=await fetch(api_link)
+	const datapoints=await response.json();
+	for (let account in datapoints.data){
+		for(let date in datapoints.data[account]["pvp"]){
+			Dates.push(date);
+			BattlesSurvived.push(datapoints.data[account]["pvp"][date]["survived_battles"])
+		}
+	}
+}
+
+async function WinLossRatio(){
+	const api_link='https://api.worldofwarships.com/wows/account/info/?application_id=40c89c194464b943f74c5d5044cc66ed&account_id=1011213121';
+	const response=await fetch(api_link)
+	const datapoints=await response.json();
+	for(let account in datapoints.data){
+		WinsPVP.push(datapoints.data[account]["statistics"]["pvp"]["wins"])
+		LossesPVP.push(datapoints.data[account]["statistics"]["pvp"]["losses"])
+	}
+}
