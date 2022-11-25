314ed252a237d0ccd400fd1a032c0d643aae44b3
Nimrod2019 <47126482+Nimrod2019@users.noreply.github.com>
2022-11-25 13:51:24 -0500

    Update apps.js

--- apps.js
+++ apps.js
@@ -21,11 +21,16 @@ async function SurvivedBattles(){
 }
 
 async function WinLossRatio(){
-	const api_link='https://api.worldofwarships.com/wows/account/info/?application_id=40c89c194464b943f74c5d5044cc66ed&account_id=1011213121';
+	const api_link='https://api.worldofwarships.com/wows/account/info/?application_id=40c89c194464b943f74c5d5044cc66ed&account_id=1008221991';
 	const response=await fetch(api_link)
 	const datapoints=await response.json();
 	for(let account in datapoints.data){
 		WinsPVP.push(datapoints.data[account]["statistics"]["pvp"]["wins"])
 		LossesPVP.push(datapoints.data[account]["statistics"]["pvp"]["losses"])
 	}
+async function statsGoozombies(){
+	const api_link='https://api.worldofwarships.com/wows/account/info/?application_id=40c89c194464b943f74c5d5044cc66ed&account_id=1008221991';
+	const response=await fetch(api_link)
+	const datapoints=await response.json();
+}
 }
